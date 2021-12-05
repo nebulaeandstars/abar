d8c17162f0f8c06a25f1ea5f73d492993f95fed1
Laura Powles <laura@laurapowles.com>
2021-12-05 17:18:58 +1100

    moved the StatusBlock cache into a new structure

---
+++ src/statusblock/cache.rs
@@ -0,0 +1,114 @@
+use std::time::{Duration, Instant};
+
+use crate::threadpool::{JobsSender, ResultsReceiver};
+
+pub struct TimedCache<T> {
+    value:           Option<T>,
+    function:        fn() -> T,
+    last_update:     Instant,
+    update_interval: Option<Duration>,
+    jobs_tx:         Option<JobsSender>,
+    results_rx:      Option<ResultsReceiver>,
+}
+
+impl<T> TimedCache<T> {
+    pub fn new(update_interval: Option<Duration>, f: fn() -> T) -> Self {
+        Self {
+            value: None,
+            function: f,
+            last_update: Instant::now(),
+            update_interval,
+            jobs_tx: None,
+            results_rx: None,
+        }
+    }
+
+    pub fn get(&mut self) -> &T {
+        self.update();
+        self.value.as_ref().unwrap().clone()
+    }
+
+    pub fn get_mut(&mut self) -> &mut T {
+        self.update();
+        self.value.as_mut().unwrap()
+    }
+
+    pub fn update(&mut self) {
+        let now = Instant::now();
+        let time_in_cache = now.duration_since(self.last_update);
+
+        let time_for_update = self.update_interval.is_some()
+            && time_in_cache > self.update_interval.unwrap();
+
+        if self.value.is_none() || time_for_update {
+            self.update_now();
+        }
+    }
+
+    pub fn update_now(&mut self) {
+        let value = (self.function)();
+        self.value = Some(value);
+        self.last_update = Instant::now();
+    }
+}
+
+impl<T: Clone + Default> Default for TimedCache<T> {
+    fn default() -> Self {
+        Self {
+            value:           None,
+            function:        T::default,
+            last_update:     Instant::now(),
+            update_interval: None,
+            jobs_tx:         None,
+            results_rx:      None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cache_evaluates_on_get() {
+        let interval = Duration::from_secs(1);
+        let mut cache = TimedCache::new(Some(interval), || 1 + 1);
+
+        assert_eq!(*cache.get(), 2);
+    }
+
+    #[test]
+    fn cache_evaluates_after_update_interval() {
+        let interval = Duration::from_millis(100);
+        let mut cache = TimedCache::new(Some(interval), || Instant::now());
+
+        let first_value = *cache.get();
+
+        std::thread::sleep(Duration::from_millis(50));
+        assert_eq!(first_value, *cache.get());
+
+        std::thread::sleep(Duration::from_millis(50));
+        assert_ne!(first_value, *cache.get());
+    }
+
+    #[test]
+    fn cache_evaluates_when_forced() {
+        let interval = Duration::from_millis(100);
+        let mut cache = TimedCache::new(Some(interval), || Instant::now());
+
+        let first_value = *cache.get();
+
+        cache.update_now();
+        assert_ne!(first_value, *cache.get());
+    }
+
+    #[test]
+    fn cache_does_not_update_when_update_interval_is_none() {
+        let mut cache = TimedCache::new(None, || Instant::now());
+        let first_value = *cache.get();
+
+        assert_eq!(first_value, *cache.get());
+        assert_eq!(first_value, *cache.get());
+        assert_eq!(first_value, *cache.get());
+    }
+}
---
+++ src/statusblock/mod.rs
@@ -0,0 +1,108 @@
+mod builder;
+mod cache;
+
+use std::fmt;
+use std::sync::Mutex;
+use std::time::Duration;
+
+pub use builder::StatusBlockBuilder;
+use cache::TimedCache;
+
+#[derive(Default)]
+pub struct StatusBlock {
+    pub name:     Option<String>,
+    pub min_size: Option<usize>,
+    pub max_size: Option<usize>,
+    cache:        Mutex<TimedCache<String>>,
+}
+
+impl StatusBlock {
+    fn new(f: fn() -> String, interval: Option<Duration>) -> Self {
+        Self {
+            cache: Mutex::new(TimedCache::new(interval, f)),
+            ..Default::default()
+        }
+    }
+
+    /// Updates the StatusBlock iff it's scheduled to be updated.
+    pub fn update(&self) {
+        let mut cache = self.cache.lock().unwrap();
+        cache.update();
+
+        drop(cache);
+        self.truncate_cache()
+    }
+
+    /// Updates the StatusBlock immediately, ignoring the timer.
+    pub fn update_now(&self) {
+        let mut cache = self.cache.lock().unwrap();
+        cache.update_now();
+
+        drop(cache);
+        self.truncate_cache()
+    }
+
+    fn truncate_cache(&self) {
+        let mut cache = self.cache.lock().unwrap();
+        let inner = cache.get_mut();
+
+        if let Some(max) = self.max_size {
+            inner.truncate(max);
+        }
+        if let Some(min) = self.min_size {
+            if inner.len() < min {
+                inner.push_str(&" ".repeat(min - inner.len()))
+            }
+        }
+    }
+}
+
+impl fmt::Display for StatusBlock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut cache = self.cache.lock().unwrap();
+        cache.update();
+        write!(f, "{}", cache.get())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_has_correct_fields() {
+        let block = StatusBlock::default();
+        assert_eq!(block.name, None);
+        assert_eq!(block.min_size, None);
+        assert_eq!(block.max_size, None);
+        assert_eq!(block.to_string(), "");
+    }
+
+    #[test]
+    fn display_draws_the_cache() {
+        let mut block = StatusBlock::new(|| String::from("test"), None);
+        assert_eq!(block.to_string(), "test");
+    }
+
+    #[test]
+    fn max_size_is_respected() {
+        let mut block =
+            StatusBlock::new(|| String::from("a very long string"), None);
+
+        block.max_size = Some(10);
+
+        block.update_now();
+        assert_eq!(block.to_string(), "a very lon");
+    }
+
+    #[test]
+    fn min_size_is_respected() {
+        let mut block =
+            StatusBlock::new(|| String::from("a short string"), None);
+
+        block.min_size = Some(20);
+
+        block.update_now();
+        assert_eq!(block.to_string(), "a short string      ");
+    }
+}
